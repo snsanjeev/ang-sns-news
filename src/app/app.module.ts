a40b6041f4330d32aa1bf23f9ab5d0503a4e172e
Sanjeev <snsanjeev@gmail.com>
2022-07-23 23:48:03 +0530

    Added validation directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { LoginPageComponent } from './screens/login-page/login-page.component';
 import { RegisterPageComponent } from './screens/register-page/register-page.component';
 import { ApplyLoanFormComponent } from './components/apply-loan/apply-loan.component';
 import { ApplyLoanComponent } from './screens/apply-loan/apply-loan.component';
+import { VerifyTaxIdDirective } from './directives/verify-tax-id.directive';
 
 // All prod and non prod
 const interceptors = [
@@ -70,7 +71,8 @@ if (!environment.production) {
     LoginPageComponent,
     RegisterPageComponent,
     ApplyLoanComponent,
-    ApplyLoanFormComponent
+    ApplyLoanFormComponent,
+    VerifyTaxIdDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/directives/verify-tax-id.directive.spec.ts
@@ -0,0 +1,8 @@
+import { VerifyTaxIdDirective } from './verify-tax-id.directive';
+
+describe('VerifyTaxIdDirective', () => {
+  it('should create an instance', () => {
+    const directive = new VerifyTaxIdDirective();
+    expect(directive).toBeTruthy();
+  });
+});
