025082821bbd38f89043153c129c8a9381438db8
Sanjeev <snsanjeev@gmail.com>
2022-07-23 18:39:21 +0530

    FormArray for city and pincode in register.

--- src/app/screens/register-page/register-page.component.ts
+++ src/app/screens/register-page/register-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 import { RegisterForm } from 'src/interfaces/user';
 
@@ -22,22 +22,34 @@ export class RegisterPageComponent implements OnInit {
   // }
 
   registerForm = new FormGroup({
-    firstName : new FormControl(""),
-    lastName : new FormControl(""),
-    email : new FormControl(""),
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl(""),
     password: new FormControl(""),
-    address : new FormGroup({
-        city : new FormControl(""),
-        pincode : new FormControl()
-    })
+    address: new FormArray([ this.createNewAddress() ])
 })
 
+createNewAddress() {
+  return new FormGroup({
+    city: new FormControl(""),
+    pincode: new FormControl("")
+  })
+}
   constructor(private user: UserService) { }
 
   ngOnInit(): void {
   }
 
   register(){
-    this.user.register(this.registerForm.value as RegisterForm).subscribe(()=>{})
+    this.user.register(this.registerForm.value as RegisterForm).subscribe(() => { })
+  }
+
+  addNewAddress() {
+    const address = this.registerForm.get("address") as FormArray;
+    address.push(this.createNewAddress())
+  }
+
+  get address() {
+    return this.registerForm.get("address") as FormArray
   }
 }
