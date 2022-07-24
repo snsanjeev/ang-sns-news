7f6c8cd30a11fcf1926920294169f7431584ecde
Sanjeev <snsanjeev@gmail.com>
2022-07-24 15:48:42 +0530

    Tax id custom validation

--- src/app/directives/verify-tax-id.directive.ts
+++ src/app/directives/verify-tax-id.directive.ts
@@ -1,10 +1,34 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[verifyTaxId]'
+  selector: '[verifyTaxId]',
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: VerifyTaxIdDirective,
+    multi: true
+  }]
 })
-export class VerifyTaxIdDirective {
+export class VerifyTaxIdDirective implements Validator {
 
+  @Input("verifyTaxId") controls !: string[]
   constructor() { }
+
+  validate(form: AbstractControl<any, any>): ValidationErrors | null {
+    if (this.controls){
+      const lastName = this.controls[0]
+      const taxId = this.controls[1]
+
+      const lastNameControl = form.get(lastName)?.value?.slice(0,1).toLowerCase()
+      const taxIdControl = form.get(taxId)?.value.slice(4,5).toLowerCase()
+
+      if (lastNameControl === taxIdControl) {
+        return null
+      }
+
+      return { taxId: true}
+    }
+    return null
+  }
 
 }
