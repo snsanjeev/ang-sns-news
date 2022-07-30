74fbe9009aef5ebec03341012ce2e74b7e155c95
Sanjeev <snsanjeev@gmail.com>
2022-07-30 20:17:39 +0530

    Added other validations

--- src/app/components/apply-account-reactive-form/apply-account-reactive-form.component.ts
+++ src/app/components/apply-account-reactive-form/apply-account-reactive-form.component.ts
@@ -8,13 +8,16 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ApplyAccountReactiveFormComponent implements OnInit {
 
+  emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+
   
   accountForm = new FormGroup({
     firstName: new FormControl("", [Validators.required]),
     lastName: new FormControl("", [Validators.required]),
-    email: new FormControl("", [Validators.required]),
-    password: new FormControl("", [Validators.required]),
-    mobileNo: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required, Validators.pattern(this.emailRegex)]),
+    password: new FormControl("", [Validators.required, Validators.minLength(8), Validators.maxLength(32)]),
+    mobileNo: new FormControl("", [Validators.required, Validators.maxLength(10)]),
     age: new FormControl("", [Validators.required]),
     taxId: new FormControl("", [Validators.required]),
     checkCredit: new FormControl("", [Validators.required])
